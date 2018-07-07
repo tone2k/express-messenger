06c07784581cb115857f1a0e08aa5b2382a1a189
Patrick Ford <patrickford@msn.com>
2018-07-07 10:52:52 -0700

    Enabled signup and login

--- public/main.js
+++ public/main.js
@@ -16,7 +16,6 @@ $(() => {
 socket.on('message', addMessages);
 socket.on('delete', getMessages);
 
-
 function addMessages(message) {
     $('#messages').append(
         `<div data-id="${message._id}" class="speech-bubble">
@@ -66,9 +65,75 @@ function loginUser() {
     });
 }
 
+$("#newAcct").click(() => {
+    $(".signup-form").show();
+    $(".login-form").hide();
+})
+
 $(document).on('submit', '#login', function (e) {
     e.preventDefault();
-   $('#mainpage').show();
-   $('#signup').hide();
-   $('#login').hide();
+    // Store the user info 
+    let username = $('#loginUsername').val();
+    let password = $('#loginPassword').val();
+
+    const user = {
+      username,
+      password
+    };
+
+    $.ajax({
+        url: '/api/auth/login',
+        type: 'POST',
+        data: JSON.stringify(user),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    })
+    .done(token => {
+        localStorage.setItem("authToken", token.authToken);
+        localStorage.setItem("currentUser", user.username);    
+        $('#mainpage').show();
+        $('#signup').hide();
+        $('#login').hide();    
+    })
+    .fail(function (err) {
+        console.log(err);
+        if (err.status === 401) {
+            $('.error-message').html('Username and/or password incorrect');
+        }
+    });
+
 })
+
+$(document).on('submit', '#signup', function (e) {
+    e.preventDefault();
+    // Store the user info 
+    let username = $('#signupUsername').val();
+    let password = $('#signupPassword').val();
+
+    const user = {
+      username,
+      password
+    };
+
+    $.ajax({
+        url: '/api/users',
+        type: 'POST',
+        data: JSON.stringify(user),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    })
+    .done(() => {    
+        $('#mainpage').hide();
+        $('#signup').hide();
+        $('#login').show();    
+    })
+    .fail(function (err) {
+        console.log(err);
+        if (err.status === 401) {
+            $('.error-message').html('Username and/or password incorrect');
+        }
+    });
+})
+
